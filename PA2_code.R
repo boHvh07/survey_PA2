213cd780f07ab5bb43c74a3b5249d8dc0cff2028
Bohvh07 <bo0709@hotmail.nl>
2021-12-14 15:27:20 +0100

    scale costruction done

--- PA2_code.R
+++ PA2_code.R
@@ -1,4 +1,9 @@
 library(readr)
+library(haven)
+library(dplyr) 
+library(psych)
+
+
 data_60 <- read_csv("~/Downloads/data_60.csv")
 View(data_60)
 
@@ -47,4 +52,66 @@ summary(data60_cl) #Recoded incorrect data entries
 data60_cl$weird[data60_cl$v17 >= 10] <- 3  # outliers marked as weird in V17
 data60_cl <- data60_cl[order(-data60_cl$weird, data60_cl$id),]
 
+
+"#### Scale Construction ####"
+dfX <- data.frame (first_column  = (data60_cl$x1),
+                  second_column = (data60_cl$x2),
+                  third_column = (data60_cl$x3),
+                  fourth_column = (data60_cl$x4) 
+                )
+
+dfX <- na.exclude(dfX)
+names(dfX) = c("x1", "x2", "x3", "x4")
+round(cor(dfX), 2)
+alpha(dfX)
+
+
+dfM <- data.frame (first_column  = (data60_cl$m1),
+                   second_column = (data60_cl$m2),
+                   third_column = (data60_cl$m3),
+                   fourth_column = (data60_cl$m4),
+                   fifth_column = (data60_cl$m5)
+)
+dfM <- na.exclude(dfM)
+names(dfM) = c("m1", "m2", "m3", "m4", "m5")
+round(cor(dfM), 2)
+alpha(dfM)
+
+
+dfY <- data.frame (first_column  = (data60_cl$y1),
+                   second_column = (data60_cl$y2),
+                   third_column = (data60_cl$y3),
+                   fourth_column = (data60_cl$y4),
+                   fifth_column = (data60_cl$y5)
+)
+dfY <- na.exclude(dfY)
+names(dfY) = c("1y", "y2", "y3", "y4", "y5")
+round(cor(dfY), 2)               
+alpha(dfY)
+
+#### Solve M2 ####
+data60_cl$m2r[data60_cl$m2 == 1] <- 5
+data60_cl$m2r[data60_cl$m2 == 2] <- 4
+data60_cl$m2r[data60_cl$m2 == 3] <- 3
+data60_cl$m2r[data60_cl$m2 == 4] <- 2
+data60_cl$m2r[data60_cl$m2 == 5] <- 1
+
+dfM2 <- data.frame (first_column  = (data60_cl$m1),
+                   second_column = (data60_cl$m2r),
+                   third_column = (data60_cl$m3),
+                   fourth_column = (data60_cl$m4),
+                   fifth_column = (data60_cl$m5)
+)
+dfM2 <- na.exclude(dfM2)
+names(dfM2) = c("m1", "m2r", "m3", "m4", "m5")
+round(cor(dfM2), 2)
+alpha(dfM2)
+
+
+#### Construct new Scales ####
+data60_cl$X <- rowMeans(data60_cl[,c(2,3,4,5)])
+
+data60_cl$M <- rowMeans(data60_cl[,c(6,8,9,10,22)])
+
+data60_cl$Y <- rowMeans(data60_cl[,c(11,12,13,14,15)])
 
